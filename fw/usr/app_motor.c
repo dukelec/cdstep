19ce41174c4ef1060855a1de0883907521c9a8b8
Duke Fong <duke@dukelec.com>
2019-11-17 22:11:17 +0800

    parse pos cmd in isr

--- fw/usr/app_motor.c
+++ fw/usr/app_motor.c
@@ -22,7 +22,6 @@ static gpio_t drv_dir = { .group = DRV_DIR_GPIO_Port, .num = DRV_DIR_Pin };
 gpio_t limit_det = { .group = LIMIT_DET_GPIO_Port, .num = LIMIT_DET_Pin };
 
 static cdnet_socket_t pos_sock = { .port = 20 };
-static cdnet_socket_t speed_sock = { .port = 21 };
 
 
 // 200 step per 360 'C
@@ -55,14 +54,14 @@ static void goto_pos(int pos, int period, int accel)
 {
     tgt_pos = min(pos, max_pos);
     gpio_set_value(&drv_dir, tgt_pos >= cur_pos); // 0: -, 1: +
-    //cur_period = max_period;
     tgt_period = clip(period, min_period, max_period);
     cur_accel = clip(accel, min_accel, max_accel);
-    if (tgt_pos == cur_pos)
-        return;
-    state = ST_RUN;
-    __HAL_TIM_SET_AUTORELOAD(&htim1, cur_period);
-    __HAL_TIM_ENABLE(&htim1);
+
+    if (tgt_pos != cur_pos && state != ST_RUN) {
+        state = ST_RUN;
+        __HAL_TIM_SET_AUTORELOAD(&htim1, cur_period);
+        __HAL_TIM_ENABLE(&htim1);
+    }
 }
 
 
@@ -73,7 +72,6 @@ void app_motor_init(void)
         list_put(&cmd_free_head, &cmd_alloc[i].node);
 
     cdnet_socket_bind(&pos_sock, NULL);
-    cdnet_socket_bind(&speed_sock, NULL);
 
     //HAL_NVIC_EnableIRQ(EXTI1_IRQn);
     HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
@@ -91,9 +89,13 @@ void app_motor_init(void)
     gpio_set_value(&drv_en, 1);
     state = ST_STOP;
 
-    goto_pos(max_pos * -2, 200, 3); // go home
+    //goto_pos(max_pos * -2, 200, 3); // go home
 }
 
+// 0x00: return: pos, speed ...
+// 0x20: pos, speed, accel  (realtime)
+// 0x21: pos, speed, accel  (cmd list)
+// 0x22: sleep              (cmd list)
 
 static void pos_mode_service_routine(void)
 {
@@ -101,21 +103,27 @@ static void pos_mode_service_routine(void)
     if (!pkt)
         return;
 
-    if (pkt->len != 17 || pkt->dat[0] != 0x20) {
-        d_warn("pos mode: wrong len: %d\n", pkt->len);
+    if (pkt->dat[0] != 0x21 && pkt->dat[0] != 0x22) {
+        d_warn("pos mode: wrong cmd\n");
         list_put(&cdnet_free_pkts, &pkt->node);
         return;
     }
+
     cmd_t *cmd = list_get_entry(&cmd_free_head, cmd_t);
     if (cmd) {
-        cmd->pos = *(int *)(pkt->dat + 1);
-        cmd->period = 1000000 / *(int *)(pkt->dat + 5);
-        cmd->accel = *(int *)(pkt->dat + 9);
-        cmd->time = *(int *)(pkt->dat + 13);
-        list_put(&cmd_pend_head, &cmd->node);
-        d_debug("pos mode: %d %d(%d) %d %d (len: %d)\n",
-                cmd->pos, *(int *)(pkt->dat + 5), cmd->period,
-                cmd->accel, cmd->time, cmd_pend_head.len);
+        if (pkt->dat[0] == 0x21) {
+            cmd->pos = *(int *)(pkt->dat + 1);
+            cmd->period = 1000000 / *(int *)(pkt->dat + 5);
+            cmd->accel = *(int *)(pkt->dat + 9);
+            cmd->time = 0;
+            d_debug("pos add: %d %d(%d) %d (len: %d)\n",
+                    cmd->pos, *(int *)(pkt->dat + 5), cmd->period,
+                    cmd->accel, cmd_pend_head.len);
+        } else {
+            cmd->time = *(int *)(pkt->dat + 1);
+            d_debug("pos add time: %d (len: %d)\n", cmd->time, cmd_pend_head.len);
+        }
+        list_put_it(&cmd_pend_head, &cmd->node);
     }
     pkt->len = 2;
     pkt->dat[0] = cmd ? 0x80 : 0x81;
@@ -125,53 +133,9 @@ static void pos_mode_service_routine(void)
     cdnet_socket_sendto(&pos_sock, pkt);
 }
 
-static void speed_mode_service_routine(void)
-{
-    cdnet_packet_t *pkt = cdnet_socket_recvfrom(&speed_sock);
-    if (!pkt)
-        return;
-
-    if (pkt->dat[0] == 0x20 || pkt->dat[0] == 0x21) {
-        int speed = *(int *)(pkt->dat + 1);
-        if (pkt->dat[0] == 0x21) {
-            cur_accel = clip(*(int *)(pkt->dat + 5), min_accel, max_accel);
-            d_debug("speed mode: %d %d\n", speed, cur_accel);
-        } else {
-            d_debug("speed mode: %d\n", speed);
-        }
-
-        if (speed == 0 || gpio_get_value(&drv_dir) != (speed >= 0)) {
-            if (state == ST_RUN) {
-                tgt_period = -1; // stop now
-                d_debug("speed mode: wait stop\n");
-                while (state == ST_RUN);
-            }
-        }
-
-        if (speed != 0) {
-            int pos = speed > 0 ? max_pos : 0;
-            int period = clip(1000000 / abs(speed), min_period, max_period);
-            local_irq_disable();
-            if (state == ST_RUN) {
-                tgt_period = period;
-                tgt_pos = pos;
-            } else {
-                goto_pos(pos, period, cur_accel);
-            }
-            local_irq_enable();
-        }
-
-        while (cmd_pend_head.len)
-            list_put_begin(&cmd_free_head, list_get(&cmd_pend_head));
-    }
-    list_put(&cdnet_free_pkts, &pkt->node);
-}
-
-
 void app_motor(void)
 {
     pos_mode_service_routine();
-    speed_mode_service_routine();
 
     if (state == ST_WAIT) {
         if (get_systick() - wait_until >= 0) {
@@ -187,13 +151,6 @@ void app_motor(void)
         }
         list_put(&cmd_free_head, &cmd->node);
 
-    } else if (state == ST_RUN) {
-        cmd_t *cmd = list_entry_safe(cmd_pend_head.first, cmd_t);
-        if (cmd && !cmd->time &&
-                (gpio_get_value(&drv_dir) == (cmd->pos >= tgt_pos)))
-            end_period = clip(cmd->period, min_period, max_period);
-        else
-            end_period = INIT_PERIOD;
     }
 }
 
@@ -204,15 +161,31 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
     gpio_get_value(&drv_dir) ? cur_pos++ : cur_pos--;
 
     if (cur_pos == tgt_pos) {
+        cmd_t *cmd = list_entry_safe(cmd_pend_head.first, cmd_t);
         d_debug("tim: arrive pos %d, period: %d\n", tgt_pos, cur_period);
-        state = ST_STOP;
-        return;
+        if (cmd && !cmd->time) {
+            goto_pos(cmd->pos, cmd->period, cmd->accel);
+            list_put(&cmd_free_head, list_get(&cmd_pend_head));
+        } else {
+            cur_period = INIT_PERIOD;
+            state = ST_STOP;
+            return;
+        }
+    }
+
+    {
+        cmd_t *cmd = list_entry_safe(cmd_pend_head.first, cmd_t);
+        if (cmd && !cmd->time && (gpio_get_value(&drv_dir) == (cmd->pos >= tgt_pos)))
+            end_period = clip(cmd->period, min_period, max_period);
+        else
+            end_period = INIT_PERIOD;
     }
 
     if (tgt_period == -1) {
         cur_period += min(abs(max_period - cur_period), cur_accel);
         if (cur_period >= max_period) {
             d_debug("tim: early stop at %d, target: %d\n", cur_pos, tgt_pos);
+            cur_period = INIT_PERIOD;
             state = ST_STOP;
             return;
         }
@@ -239,5 +212,5 @@ void limit_det_isr(void)
     __HAL_TIM_CLEAR_IT(&htim1, TIM_IT_UPDATE);
     cur_period = max_period;
     cur_pos = -1000;
-    goto_pos(0, 50, 3);
+    //goto_pos(0, 50, 3);
 }
