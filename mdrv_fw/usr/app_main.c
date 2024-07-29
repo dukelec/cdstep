b125ff270910dd3610a1f301ebc9d6c79da21df3
Duke <d@d-l.io>
2024-07-29 16:09:39 +0800

    check all led during power on

--- mdrv_fw/usr/app_main.c
+++ mdrv_fw/usr/app_main.c
@@ -122,6 +122,8 @@ static void dump_hw_status(void)
 
 void app_main(void)
 {
+    gpio_set_value(&led_r, 1);
+    gpio_set_value(&led_g, 1);
     printf("\nstart app_main (mdrv-step)...\n");
     stack_check_init();
     load_conf();
@@ -135,7 +137,8 @@ void app_main(void)
     csa_list_show();
     app_motor_init();
 
-    gpio_set_value(&led_g, 1);
+    delay_systick(100);
+    gpio_set_value(&led_r, 0);
     //uint32_t t_last = get_systick();
     while (true) {
         //if (get_systick() - t_last > (gpio_get_value(&led_g) ? 400 : 600)) {
